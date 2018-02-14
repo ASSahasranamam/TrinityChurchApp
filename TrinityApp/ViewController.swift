98fb4fda8191bbfa278158d33196cb13ca3eb5cf
user135588 <user135588@ny436.local>
2018-02-14 14:04:51 -0800

    Initial Commit

---
+++ TrinityApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrinityApp
+//
+//  Created by user135588 on 2/14/18.
+//  Copyright © 2018 tlc4u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
